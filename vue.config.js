23ab9b72d29588159d25d571a9ebf3f3bd5a2112
Barrior <barrior@qq.com>
2022-12-20 16:18:56 +0800

    chore: build site

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/lucky-draw/docs' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/lucky-draw' : '/'
 };
